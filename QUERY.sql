4643297c205473055161f9f4bad24e18adb45f1a
MahtabAshfi <mahtabashfi1625@gmail.com>
2026-06-15 13:57:05 +0600

    completed query 5

--- QUERY.sql
+++ QUERY.sql
@@ -123,3 +123,14 @@ FROM
   Bookings b
   INNER JOIN Users u ON b.user_id = u.user_id
   INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- =========================================================================
+-- QUERY 5: All users with their booking IDs
+-- =========================================================================
+SELECT
+  u.user_id,
+  u.full_name,
+  b.booking_id
+FROM
+  Users u
+  LEFT JOIN Bookings b ON u.user_id = b.user_id
